17f944dbfb392bdd59ef91ac20cac043162e03e1
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-09-16 22:35:30 +0300

    Suleyman Poyraz Stili hello world :joy:

---
+++ Helloworld/app/src/main/java/com/example/helloworld/Hello.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+
+}
